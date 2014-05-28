c116d28e6bf24424fc5e4ffe7a530c6d60965029
Tim Snadden <tim@snadden.com>
2014-05-29 01:28:25 +1200

    Update for silverstripe 3.1. Set vars to private, move config into yaml.

--- code/SecureFileAccessToken.php
+++ code/SecureFileAccessToken.php
@@ -6,45 +6,32 @@
  * @package securefiles
  * @subpackage default
  * @author Hamish Campbell <hn.campbell@gmail.com>
- * @copyright copyright (c) 2010, Hamish Campbell 
+ * @copyright copyright (c) 2010, Hamish Campbell
  */
 class SecureFileAccessToken extends DataObject {
-	
-	static $singular_name = "File Access Token";
-	
-	static $plural_name = "File Access Tokens";
-	
-	static $db = array(
+
+	private static $db = array(
 		'Token' => 'Varchar(32)',
 		'Expiry' => 'Datetime',
 	);
-	
-	static $has_one = array(
+
+	private static $has_one = array(
 		'File' => 'File',
 		'Member' => 'Member',
 	);
-	
-	static $has_many = array(
-	);
-	
-	static $many_many = array(
-	);
-	
-	static $belongs_many_many = array(
-	);
-	
-	static $summary_fields = array(
-		'Name' => 'Name', 
+
+	private static $summary_fields = array(
+		'Name' => 'Name',
 		'MemberNice' => 'Member',
-		'ExpiryNice' => 'Expiry', 
+		'ExpiryNice' => 'Expiry',
 		'TokenedAbsoluteURL' => 'URL'
 	);
-	
-	static $casting = array(
+
+	private static $casting = array(
 		'ExpiryNice' => 'Varchar',
 		'MemberNice' => 'Varchar',
 	);
-	
+
 	/**
 	 * Get the file name for this access token record
 	 * @return string
@@ -52,15 +39,15 @@ class SecureFileAccessToken extends DataObject {
 	public function getName() {
 		return $this->File()->Name;
 	}
-	
+
 	/**
 	 * Get the URL for this access token record
 	 * @return string
 	 */
 	public function getURL() {
-		return $this->File()->URL;		
+		return $this->File()->URL;
 	}
-	
+
 	/**
 	 * Get the absolute tokened URL path to access this file
 	 * @return string
@@ -68,7 +55,7 @@ class SecureFileAccessToken extends DataObject {
 	public function getTokenedAbsoluteURL() {
 		return Controller::join_links(Director::absoluteBaseURL() . $this->File()->getFilename(), "?token=" . $this->Token);
 	}
-	
+
 	/**
 	 * Get the relative tokened URL path to access this file
 	 * @return string
@@ -76,7 +63,7 @@ class SecureFileAccessToken extends DataObject {
 	public function getTokenedURL() {
 		return Controller::join_links($this->File()->URL, "?token=" . $this->Token);
 	}
-	
+
 	/**
 	 * Return a nicely formatted expiry time
 	 * @return string
@@ -89,7 +76,7 @@ class SecureFileAccessToken extends DataObject {
 			return _t('SecureFiles.NEVEREXPIRES', 'Never');
 		}
 	}
-	
+
 	/**
 	 * Return a nicely formatted description of the member who has access
 	 * @return string
@@ -100,7 +87,7 @@ class SecureFileAccessToken extends DataObject {
 		else
 			return _t('SecureFiles.EVERYONE', 'Everyone');
 	}
-	
+
 	/**
 	 * @return GridFieldConfig A config suitable for displaying a list of SecureFileAccessTokens.
 	 */
@@ -114,23 +101,23 @@ class SecureFileAccessToken extends DataObject {
 		$config->addComponent(new GridFieldDeleteAction());
 		$config->addComponent(new GridFieldPaginator(30));
 		$config->addComponent(new GridFieldDetailForm());
-		
+
 		return $config;
 	}
-	
+
 	function getCMSFields() {
 		$fields = new FieldList();
 		$fields->push($expiry_field = new DatetimeField('Expiry', 'Expiry'));
 		$expiry_field->getDateField()->setConfig('showcalendar', true);
 		$expiry_field->getTimeField()->setConfig('showdropdown', true);
-		
+
 		$fields->push(new ReadonlyField('MemberDummyField', 'Member', $this->MemberNice()));
 		if($this->ID)
 			$fields->push(new ReadonlyField('Token', 'Token'));
 		$this->extend('updateCMSFields', $fields);
 		return $fields;
 	}
-	
+
 	/**
 	 * Generate a unique and hard to guess MD5 hash for this token.
 	 * @return string of 32 characters
@@ -138,7 +125,7 @@ class SecureFileAccessToken extends DataObject {
 	protected function generateHash() {
 		return md5(microtime(true).rand(100000, 999999));
 	}
-	
+
 	/**
 	 * After writing this object, check if it has a valid token. If not, generate one!
 	 */
@@ -147,7 +134,7 @@ class SecureFileAccessToken extends DataObject {
 			$this->Token = $this->generateHash();
 		parent::onBeforeWrite();
 	}
-	
-	
-	
+
+
+
 }
--- code/SecureFileCheckFoldersTask.php
+++ code/SecureFileCheckFoldersTask.php
@@ -8,37 +8,37 @@
  * @package securefiles
  * @subpackage tasks
  * @author Hamish Campbell <hn.campbell@gmail.com>
- * @copyright copyright (c) 2010, Hamish Campbell 
+ * @copyright copyright (c) 2010, Hamish Campbell
  */
 class SecureFileCheckFolders extends BuildTask {
-	
+
 	protected $title = 'Apply Secure File Access Rules';
-	
+
 	protected $description = 'Traverses your asset file structure rebuilding/removing htaccess rules as appropriate.';
-	
+
 	public function run($request) {
 		if(!Director::is_cli()) echo "<pre>";
 		echo "* Sync filesystem with database...\n";
 		echo "* " . FileSystem::sync() . "\n";
 		echo "* Applying secure file rules...\n";
-		
+
 		$secure = 0; $unsecure = 0;
 		$folders = DataObject::get('Folder');
-		
+
 		if($folders) {
 			foreach($folders as $folder) {
 				$folder->Secured ? $secure++ : $unsecure++;
 				$folder->forceChange();
 				$folder->write();
-				
+
 			}
 			echo "* " . $folders->Count() . " folders processed:  $secure secure, $unsecure unsecure\n";
 		} else {
 			echo "* No folders found!\n";
 		}
-		
+
 		echo "* Task finished.\n";
 		if(!Director::is_cli()) echo "</pre>";
 	}
-	
+
 }
